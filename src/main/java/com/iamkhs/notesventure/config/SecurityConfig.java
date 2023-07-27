a8dcfdecd96d723a7795e10279636e1be95c3c6a
Kibriar Hossain <shantohossain.p@gmail.com>
2023-07-28 02:07:14 +0600

    SecurityConfig file updated

--- src/main/java/com/iamkhs/notesventure/config/SecurityConfig.java
+++ src/main/java/com/iamkhs/notesventure/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.iamkhs.notesventure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -39,10 +38,16 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth->
                         auth.requestMatchers("/user/**").hasRole("USER")
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/**").permitAll())
-                .formLogin(Customizer.withDefaults())
+
+                .formLogin(form-> form.loginPage("/login")
+                        .loginProcessingUrl("/do-login")
+                        .defaultSuccessUrl("/user/0/u/dashboard"))
+
                 .authenticationProvider(daoAuthenticationProvider());
 
         return http.build();
